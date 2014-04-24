c071586a4d558b5d0d9da77baef47334f2e4899a
Rich Salvucci <richsalvucci@mac.com>
2014-04-24 11:54:39 -0400

    Seperated products form stores

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,5 @@
 class Product < ActiveRecord::Base
-  has_many :checkouts
   has_many :carts
-  has_many :stores, through: :checkouts
   validates :name, presence: true, length: {minimum: 5}
 
 end 
